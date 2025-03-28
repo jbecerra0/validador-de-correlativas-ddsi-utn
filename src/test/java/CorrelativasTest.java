ded5017c48d33960bef6a5af6ec1e348b8dcbeb2
jbecerra0 <bejose34@gmail.com>
2025-03-28 08:24:24 -0300

    Cambio la composición por una asociación simple.

--- src/test/java/CorrelativasTest.java
+++ src/test/java/CorrelativasTest.java
@@ -3,7 +3,6 @@ import org.example.Inscripcion;
 import org.example.Materia;
 import org.junit.Test;
 
-import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.Assert.assertFalse;
